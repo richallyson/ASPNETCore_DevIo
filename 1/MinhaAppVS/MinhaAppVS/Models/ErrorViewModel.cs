a228fa68a62dd98c272fc6ce415fcfa779dce34b
richallyson <richallyson@gmail.com>
2022-08-31 18:25:44 -0300

    Finalizando o curso Iniciado ASP.NET Core

---
+++ 1/MinhaAppVS/MinhaAppVS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MinhaAppVS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ 1/MinhaAppVsCode/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MinhaAppVsCode.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
